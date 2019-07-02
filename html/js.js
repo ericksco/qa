1d00d090bf8b18befa547037cd38db3831cf686c
Cory Erickson <ericksco@fozzy.erickson.org>
2019-07-02 00:42:42 -0500

    push

--- html/js.js
+++ html/js.js
@@ -4,7 +4,7 @@ var bob = "hello there";
 var x;
 for (x = 0; x < geo.length; x++)
 {
-	#window.alert("state = " + toString(geo[x].state));
+	// window.alert("state = " + toString(geo[x].state));
 	window.alert("state = " + geo[x].state);
 }
 
