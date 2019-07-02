4c5dfbca4295195dc643897c995aa6cac9268ac4
Cory Erickson <ericksco@fozzy.erickson.org>
2019-07-02 00:36:17 -0500

    push

--- html/js.js
+++ html/js.js
@@ -1,49 +1,12 @@
 
 var bob = "hello there";
-//document.getElementById("bob").value = bob;
 
 var x;
 for (x = 0; x < geo.length; x++)
 {
-	window.alert("state = " + toString(geo[x].state));
+	#window.alert("state = " + toString(geo[x].state));
+	window.alert("state = " + geo[x].state);
 }
 
 document.getElementById("bob").value = bob;
 
-
-/*
-        var total = null;
-        var calctype = "plus";
-        var lastop = "add";
-        var cleardisplay = 1;
-
-        var nextoperator = 0;
-        var last = 0;
-        var buffer = 0;
-
-        var stringOne = "hello there";
-        var stringTwo = "hey dummy";
-        var myArray = ["Steve", "Ben", "Cory", "Katie", "Kevin"];
-        var concatString1 = "beginning " + stringOne + stringTwo + " end";
-</script>
-
-<script type="text/javascript">
-        function saveoperator(operator)
-        {
-                lastop = operator;
-                return;
-        }
-
-        function clicknumber(value)
-        {
-                nextoperator = 1;
-                if (cleardisplay == 1)
-                {
-                        updatedisplay(null);
-                        cleardisplay = 0;
-                }
-                newvalue = document.getElementById("display").value * 10 + value;
-                updatedisplay(newvalue);
-                return;
-        }
-*/
