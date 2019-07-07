f07d2f7171316c7cd509ac400a1ceebfbb5b26a6
Cory Erickson <ericksco@fozzy.erickson.org>
2019-07-07 10:20:13 -0500

    push

--- html/js.js
+++ html/js.js
@@ -1,10 +1,17 @@
 
 var bob = "hello there";
 
+/*
 var x;
 for (x = 0; x < geo.length; x++)
 {
 	window.alert("state = " + geo[x].state);
+}
+*/
+
+function changeState()
+{
+	window.alert("state changed to" + document.getElementById("states").value);
 }
 
 document.getElementById("bob").value = bob;
