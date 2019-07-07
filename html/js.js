ca2c9e9a9b1a73b20e465e80ce9b891e84f7da4d
Cory Erickson <ericksco@fozzy.erickson.org>
2019-07-07 12:29:28 -0500

    more bob troubleshooting

--- html/js.js
+++ html/js.js
@@ -11,8 +11,27 @@ for (x = 0; x < geo.length; x++)
 
 function changeState()
 {
-	var stateSelectObject = document.getElementById("states");
-	window.alert("state changed to " + stateSelectObject.value + " " + document.getElementById("bob").innerHTML);
+	var statesSelectObject = document.getElementById("states");
+	window.alert("state changed to " + statesSelectObject.value + " " + document.getElementById("bob").innerHTML);
+
+	// empty out the cities select box
+	var cities = document.getElementById("cities");
+	
+	var x;
+	for (x = 0; x < cities.length; x++)
+	{
+		cities.remove(x);
+	}
+	
+	// add cities for the state into the cities select box
+	
+	// geo["north dakota"] = [ ];
+	
+	var mystate = statesSelectObject.value;
+	for (x = 0; x < geo[mystate].cities.length; x++)
+	{
+		cities.add(geo[mystate].cities[x]);
+	} 
 }
 
 document.getElementById("bob").innerHTML = "fred";
